a8cf30f3bf62833b00994a6bc1377f87b237d016
trentonsoto <sotot946@newschool.edu>
2026-04-04 13:53:29 -0400

    removed js pt.II

--- assets/form-state.js
+++ assets/form-state.js
@@ -1,6 +1,6 @@
 // Target your form.
 let formElement = document.querySelector('#some-form')
-let stateCallback = undefined
+// let stateCallback = undefined
 // I added this because of an error code in my devtools. I will come back to this later, but I needed to get this out the way to make sure my sessions are saved locally.
 
 
@@ -23,9 +23,4 @@ formElement.addEventListener('submit', (event) => {
 	event.preventDefault()
 })
 
-// EVERY SINGLE TIME ITS CHANGED, ITS FIRING AN EVENT
-// Run any time the form is modified:
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
-formElement.addEventListener('input', () => {
-	updateUrlParams()
-})
+
